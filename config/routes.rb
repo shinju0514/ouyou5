8a5e223a6dea8956368015db3ca837c51a4b6a56
shinju0514 <shinju0514yellow@gmail.com>
2022-04-02 10:20:31 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about" ,as: "about"
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+   resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+ end
   resources :users, only: [:index,:show,:edit,:update]
 end
